c5e2bd9391e94dfe71c4e410395bf7b268f3ee14
mahesh-j <mahesh.j13@gmail.com>
2018-08-07 11:53:28 +0000

    Annotation changes for Dropdown in smartfilter bar

--- ui/resources/webapp/controller/Carrier.controller.js
+++ ui/resources/webapp/controller/Carrier.controller.js
@@ -4,7 +4,13 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("ui.ui.controller.Carrier", {
-		init : function() {
+		onInit : function() {
+			/*this._odataModel = new sap.ui.model.odata.v2.ODataModel(
+				"/service.xsodata", {
+					annotationURI: "annotation/annotation.xml"
+				}
+			);
+			this.getView().setModel(this._odataModel);*/
 			console.log('This is onInit');
 		},
 		
